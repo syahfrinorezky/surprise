9476becf2885ca6f51e3cb36c03902c813ebd575
syahfrinorezky <syahfrinorezky@gmail.com>
2025-05-25 19:17:27 +0800

    feat(data): add main data

---
+++ src/data/relationship.data.ts
@@ -0,0 +1,15 @@
+import { RelationshipProps } from "@/lib/types/relationship.types";
+
+export const relationship: RelationshipProps = {
+  name: {
+    bf: "Syahfrino Rezky Oktaviant",
+    gf: "Sharen Jasminycta Azadira Syahputri",
+  },
+  birthdate: {
+    bf: "2005-10-08",
+    gf: "2005-04-11",
+  },
+  relationship: {
+    startdate: "2023-02-22",
+  },
+};
